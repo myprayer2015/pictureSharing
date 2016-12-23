08c9e81c4e95a0f3afaa7d01ebf9c406e96815ec
xue <xuexiaodong161@mails.ucas.ac.cn>
2016-12-24 00:16:49 +0800

    complete_database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
 	def index
+
+	end
+	def show
+
 	end
 	def new
 		@user = User.new
+	end
+	def create
+		params.permit!
+		@user = User.new(params[:user])
+		if@user.save
+			session['loginedUser'] = @user
+			redirect_to :controller=>'home',:action=>'index'
+		else
+			redirect_to :controller=>'home', action=>'index'
+	end
+	def edit
+
+	end
+	def update
+
+	end
+	def destroy
+
 	end
 end
